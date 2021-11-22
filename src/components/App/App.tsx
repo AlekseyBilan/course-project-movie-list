f96b7806d018a8d1cb2ed589d402d21474c85b30
oleksii_bilan <aleksey.bilan@rmgmedia.com>
2021-11-22 12:03:45 +0200

    cleen up all demo functionality

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,9 @@
 import './App.scss';
 import * as React from 'react';
-import Title from '../Title/Title';
-import Checkbox from '../Checkbox/Checkbox';
-import Checkbox2 from '../Checkbox/Checkbox2';
-import { Switch, Route, BrowserRouter as Router, Link } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 import NotFound from '../NotFound/NotFound';
-import UseState from '../Hooks/UseState';
 import Search from '../Search/Search';
-import UseEffect from '../Hooks/UseEffect';
-import UseRef from '../Hooks/UseRef';
 import Sort from '../Sort/Sort';
-import Test from '../Test';
 
 interface IProps {
 }
@@ -60,38 +53,9 @@ class App extends React.Component<IProps, IState> {
     return (  
       <Router>
         <Switch>
-          <Route path='/search'>
-            <Search searchHandler={this.searchHandler}/>
-          </Route>
-          <Route path='/checkbox2'>
-            <Checkbox2 onChangeHendler={this.checkedHandler} isChecked={this.state.isChecked}/>
-          </Route>
           <Route path='/home'>
             <Search searchHandler={this.searchHandler}/>
             <Sort items={this.state.items}/>
-          </Route>
-          <Route path='/usestate'>
-            <UseState/>
-          </Route>
-          <Route path='/useeffect'>
-            <UseEffect/>
-          </Route>
-          <Route path='/useref'>
-            <UseRef/>
-          </Route>
-          <Route path='/test'>
-            <Test/>
-          </Route>
-          <Route path='/checkbox'>
-            <Title titleText={'Welcome on Checkbox example'} />
-            <Checkbox checkedHandler={this.checkedHandler} isChecked />
-            <Link to='/home'>go home</Link>
-          </Route>        
-          <Route path='/'>
-            <Title titleText={'Welcome at Movie-list App'} />
-            <Link to='/home'>go home</Link>
-            <br/>
-            <Link to='/checkbox'>go checkbox</Link>
           </Route>
           <Route path='*' component={NotFound}/>
         </Switch>
--- src/components/Test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Test() {
-    return (
-        <svg id="icon-sort-ascending" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24" xmlXpace="preserve">
-            <circle cx="10" cy="10" r="10" />
-        </svg>
-    );
-}
